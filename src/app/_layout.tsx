faca15569ccb8804bf953bf405bd902aba01617a
Leonardo de Faveri <faver_i@hotmail.com>
2024-09-24 22:26:09 -0300

    create Home screen and separate from root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,5 @@
-import { Text, View } from "react-native";
+import Home from "@/screens/Home";
 
 export default function RootLayout() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello Im'Here</Text>
-    </View>
-  );
+  return <Home />;
 }
